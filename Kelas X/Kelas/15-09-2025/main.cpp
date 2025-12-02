2432580e16e1c97aba772f1b1c319b807759cb48
stellamariap <stellamaria.putroromo@gmail.com>
2025-12-02 13:44:44 +0700

    Add files via upload

---
+++ Kelas X/Kelas/15-09-2025/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout <<"Program Luas Persegi"<<endl;
+    int l,p,luas;
+    cout<<"Masukkan Panjang = ";
+    cin>>p;
+    cout<<"Masukkan Lebar = ";
+    cin>>l;
+    luas = p*l;
+    cout<<"Luas Persegi Panjang = "<<luas<<endl;
+    return 0;
+    
+}
