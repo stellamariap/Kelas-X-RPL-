459c9c82a7b5fe1fa499454d0ad903e7d8ce275b
stellamariap <stellamaria.putroromo@gmail.com>
2026-01-12 09:42:48 +0700

    Add files via upload

---
+++ Kelas X/Kelas/Kelas Pak Tri/12-01-2026/XRpl.java
@@ -0,0 +1,39 @@
+
+public class XRpl {
+
+  
+    public static void main(String[] args) {
+        int angka;
+        angka=10;
+        
+        System.out.println(angka);
+        System.out.println("Angka yang anda inputkan: "+angka);
+        angka=100;
+        System.out.println("Angka terbaru adalah: "+angka);
+        angka=1000;
+        System.out.println("Angka yang anda inputkan: " +angka);
+        angka=10000; 
+        System.out.println("Angka yang anda inputkan: " +angka);
+        angka=100000;
+        System.out.println("Angka yang anda inputkan: " +angka);
+        
+        double desimal=10.355;
+        float pecahan= 13.4555f;
+        System.out.println(desimal);
+        System.out.println("Angka pecahan yang anda inputkan: "+pecahan);
+        
+        String nama="Yemima"; 
+        char   abjad='A';
+        System.out.println(nama);
+        System.out.println(abjad);
+        
+        boolean betul = true; 
+        boolean salah = false; 
+        System.out.println(betul);
+        System.out.println(salah);
+        
+//        desimal= 100.235; 
+        System.out.println(desimal);
+    }
+    
+}
