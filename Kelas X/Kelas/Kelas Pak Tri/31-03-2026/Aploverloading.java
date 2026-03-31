175200e720df8a0dd1f4f3919c9cf29e9d1c975c
stellamariap <stellamaria.putroromo@gmail.com>
2026-03-31 14:09:58 +0700

    Add files via upload

---
+++ Kelas X/Kelas/Kelas Pak Tri/31-03-2026/Aploverloading.java
@@ -0,0 +1,20 @@
+
+package belajaroop;
+
+public class Aploverloading {
+
+    public static void main(String[] args) {
+//      OverLoading objek= new OverLoading();
+//      objek.DataKelas("Sepuluh RPL");
+//      objek.DataKelas("Sepuluh RPL", 36);
+
+//    OverLoading objek= new OverLoading();
+//    objek.DataInstagram("xerpeel");
+//    objek.DataInstagram("xerpeel", 999);
+//    
+    
+    OverLoading obj= new OverLoading();
+    obj.DataInstagram("xerpeel");
+    obj.DataInstagram("xerpeel", 999);
+    }
+}
---
+++ Kelas X/Kelas/Kelas Pak Tri/31-03-2026/OverLoading.java
@@ -0,0 +1,31 @@
+
+package belajaroop;
+
+
+public class OverLoading {
+    
+//    void DataKelas(String nmkelas){
+//        System.out.println("Kelas Kita Adalah: " +nmkelas);
+//    } 
+//    void DataKelas(String nmkelas,int jmlsiswa){
+//            System.out.println("Kelas Kita Adalah: " +nmkelas);
+//            System.out.println("Jumlah Sobat kita adalah: " +jmlsiswa+ " siswa");
+
+//    }
+    
+//        void DataInstagram (String nminstagram){
+//            System.out.println("Nama Instagram Kita Adalah: " +nminstagram);
+//        }
+//        void DataInstagram(String nminstagram, int jmlpengikut){
+//            System.out.println("Nama Instagram Kita Adalah: " +nminstagram);
+//            System.out.println("Jumlah Pegikut kita adalah: " +jmlpengikut+ " pengikut");
+//        }
+    
+        public void DataInstagram (String nminstagram){
+            System.out.println("Nama Instagram : " +nminstagram);
+        }
+        public void DataInstagram (String nminstagram, int jmlpengikut){
+            System.out.println("Nama Instagram : " +nminstagram);
+            System.out.println("Jumlah Pengikut Instagram : " +jmlpengikut);
+        }
+}
